8b6cf87305aff73f369b8382362bc14c95c926a5
ch0rtas <47576091+ch0rtas@users.noreply.github.com>
2025-01-31 02:06:53 +0100

    Create GithubDetect.cs

---
+++ GithubDetect.cs
@@ -0,0 +1,20 @@
+// Este archivo .cs es un marcador para que GitHub detecte que el código es C#
+// El proyecto "PINGU" es un juego de plataformas 2D desarrollado en Unity con C#.
+
+// Clase de ejemplo en C# para PINGU
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("¡Hola, bienvenido a PINGU!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Aquí se podrían agregar las mecánicas de movimiento o lógica de juego
+    }
+}
